13a979800d9d0e292693e968ebd11b8dccf4cad7
Israel Aguilar <isra.neri2@gmail.com>
2022-07-03 20:58:32 -0400

    added print in launching test application

--- hooks/pre-commit.bash
+++ hooks/pre-commit.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo “Running pre-commit hook”
-xcodebuild -quiet -project YourProject.xcodeproj -scheme “YourTestScheme” -destination 'platform=iOS Simulator,name=iPhone XR,OS=12.1' test
+xcodebuild -quiet -project social-network-app.xcodeproj -scheme “social-network-app” -destination 'platform=iOS Simulator,name=iPhone 13 Pro,OS=15.5' test
 if [ $? -ne 0 ]; then
  echo “Tests must pass before commit!”
  exit 1
